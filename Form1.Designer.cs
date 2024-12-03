138a7afc28788b7af9f45bf24a051ed69fae7dce
ImGraviti <ImGraviti@DESKTOP-53DO703>
2024-12-02 23:37:15 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace testsagabg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ArtistLabel = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtArtistName = new System.Windows.Forms.TextBox();
+            this.rtbResults = new System.Windows.Forms.RichTextBox();
+            this.TextBoxArtistName = new System.Windows.Forms.TextBox();
+            this.rtbTopTracks = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ArtistLabel
+            // 
+            this.ArtistLabel.AutoSize = true;
+            this.ArtistLabel.BackColor = System.Drawing.Color.Lavender;
+            this.ArtistLabel.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ArtistLabel.Location = new System.Drawing.Point(23, 30);
+            this.ArtistLabel.Name = "ArtistLabel";
+            this.ArtistLabel.Size = new System.Drawing.Size(116, 25);
+            this.ArtistLabel.TabIndex = 0;
+            this.ArtistLabel.Text = "Artist Name";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(21, 121);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(88, 33);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtArtistName
+            // 
+            this.txtArtistName.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArtistName.Location = new System.Drawing.Point(21, 71);
+            this.txtArtistName.Name = "txtArtistName";
+            this.txtArtistName.Size = new System.Drawing.Size(423, 33);
+            this.txtArtistName.TabIndex = 2;
+            this.txtArtistName.TextChanged += new System.EventHandler(this.txtArtistName_TextChanged);
+            // 
+            // rtbResults
+            // 
+            this.rtbResults.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbResults.Location = new System.Drawing.Point(21, 168);
+            this.rtbResults.Name = "rtbResults";
+            this.rtbResults.Size = new System.Drawing.Size(423, 257);
+            this.rtbResults.TabIndex = 3;
+            this.rtbResults.Text = "";
+            this.rtbResults.TextChanged += new System.EventHandler(this.rtbResults_TextChanged);
+            // 
+            // TextBoxArtistName
+            // 
+            this.TextBoxArtistName.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxArtistName.Location = new System.Drawing.Point(534, 71);
+            this.TextBoxArtistName.Name = "TextBoxArtistName";
+            this.TextBoxArtistName.Size = new System.Drawing.Size(244, 33);
+            this.TextBoxArtistName.TabIndex = 6;
+            this.TextBoxArtistName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // rtbTopTracks
+            // 
+            this.rtbTopTracks.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbTopTracks.Location = new System.Drawing.Point(567, 168);
+            this.rtbTopTracks.Name = "rtbTopTracks";
+            this.rtbTopTracks.Size = new System.Drawing.Size(177, 257);
+            this.rtbTopTracks.TabIndex = 7;
+            this.rtbTopTracks.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lavender;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(565, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Top Tracks / Albums";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(191, 121);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 33);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(356, 121);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(88, 33);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lavender;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(535, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Found Artist";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbTopTracks);
+            this.Controls.Add(this.TextBoxArtistName);
+            this.Controls.Add(this.rtbResults);
+            this.Controls.Add(this.txtArtistName);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.ArtistLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ArtistLabel;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtArtistName;
+        private System.Windows.Forms.RichTextBox rtbResults;
+        private System.Windows.Forms.TextBox TextBoxArtistName;
+        private System.Windows.Forms.RichTextBox rtbTopTracks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿//Project Semester CIS 022
+//Group Members: Arjeevan Samra & Luke Williams
+using IF.Lastfm.Core.Api;
+using IF.Lastfm.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
+
+namespace testsagabg
+{
+    public partial class Form1 : Form
+    {
+        private LastfmClient _client;
+        public Form1()
+        {
+            InitializeComponent();
+            _client = new LastfmClient("95331ac160a246b0ef593ce69211aa19", "50df70d0862af9013bc0a1d2920f735b");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            string artistName = txtArtistName.Text;
+            if (string.IsNullOrEmpty(artistName))
+            {
+                MessageBox.Show("Please enter an artist name.");
+                return;
+            }
+
+            try
+            {
+                var response = await _client.Artist.GetInfoAsync(artistName); // Corrected variable name
+                if (response.Success)
+                {
+                    LastArtist artist = response.Content; // Corrected property for artist details
+                    await DisplayArtistInfo(artist); // Fixed method placement and call
+                }
+                else
+                {
+                    rtbResults.Text = "Artist not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error has occurred: {ex.Message}");
+            }
+        }
+                private async Task DisplayArtistInfo(LastArtist artist)
+                {
+                    rtbResults.Clear();
+                    rtbResults.AppendText($"Artist: {artist.Name}\n");
+                    rtbResults.AppendText($"Listeners: {artist.Stats.Listeners}\n");
+                    rtbResults.AppendText($"Biography: {artist.Bio.Summary}\n\n");
+                    TextBoxArtistName.AppendText($"{artist.Name}");
+                    rtbTopTracks.AppendText("Top Tracks:\n\n\n");
+                    var topTracksResponse = await _client.Artist.GetTopTracksAsync(artist.Name);
+
+                    if (topTracksResponse.Success)
+                    {
+                        foreach (var track in topTracksResponse.Content.Take(5)) // Corrected track list access
+                        {
+                            rtbTopTracks.AppendText($"-  {track.Name}\n\n");
+                        }
+                    }
+                    else
+                    {
+                        rtbTopTracks.AppendText("Could not retrieve top tracks.");
+                    }
+            var albumResponse = await _client.Artist.GetTopAlbumsAsync(artist.Name);
+            if (albumResponse.Success)
+            {
+                rtbTopTracks.AppendText("\n\n\nTop Albums:\n\n\n");
+                foreach (var album in albumResponse.Content.Take(100)) // Corrected track list access
+                {
+                    rtbTopTracks.AppendText($"-  {album.Name}\n\n");
+                }
+            }
+            else
+            {
+                rtbTopTracks.AppendText("Could not retrieve top tracks.");
+            }
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtbResults_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtArtistName.Clear();
+            TextBoxArtistName.Clear();
+            rtbTopTracks.Clear();
+            rtbResults.Clear();
+        }
+
+        private void txtArtistName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+        }
